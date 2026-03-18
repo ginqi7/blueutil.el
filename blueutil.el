b99d71903670e404bed94b6885bf9abebb096bb7
Qiqi Jin <ginqi7@gmail.com>
2026-03-18 20:51:37 +0800

    docs(blueutil): add comprehensive docstrings and usage commentary for public/internal APIs

--- blueutil.el
+++ blueutil.el
@@ -20,16 +20,36 @@
 
 ;;; Commentary:
 
+;; Emacs package for managing Bluetooth devices on macOS using the blueutil command-line utility.
 ;;
+;; Usage:
+;;   M-x blueutil-search-paired  ; Interactive interface to manage paired devices
+;;
+;; Or programmatically:
+;;   (blueutil-paired)       ; List paired devices
+;;   (blueutil-connected)    ; List connected devices
+;;   (blueutil-connect id)   ; Connect to a device
+;;   (blueutil-disconnect id) ; Disconnect a device
 
 ;;; Code:
 
 (defcustom blueutil-command (executable-find "blueutil")
-  "")
+  "Path to the blueutil command-line executable.
 
-;;; Internal Functgions
+This variable holds the full path to the blueutil binary used for
+Bluetooth device management on macOS.  By default, it searches for
+the executable in the system PATH.")
+
+;;; Internal Functions
 
 (defun blueutil--run-json (command &optional options)
+  "Execute COMMAND with OPTIONS and parse the output as JSON.
+
+COMMAND is a symbol representing the blueutil subcommand to execute.
+OPTIONS is an optional list of additional command-line arguments.
+
+Returns the parsed JSON result as Emacs Lisp data structures,
+with arrays converted to lists and JSON booleans to nil."
   (let ((output (blueutil--run command options)))
     (json-parse-string
      output
@@ -39,28 +59,101 @@
 ;; (defun blueutil--run-str ())
 
 (defun blueutil--run (command &optional options)
+  "Execute a blueutil COMMAND with OPTIONS and return the raw output.
+
+COMMAND is a symbol representing the blueutil subcommand (e.g., 'paired, 'connected).
+OPTIONS is an optional list of additional command-line arguments.
+
+Returns the raw string output from the shell command.  The command
+is executed with --format json and the specified COMMAND and OPTIONS."
   (shell-command-to-string (format "%s --format json --%s %s" blueutil-command command (string-join options " "))))
 
 (defun blueutil--info-key (info)
+  "Generate a display key for a Bluetooth device INFO hash.
+
+INFO is a hash table containing device information with at least
+\"name\" and \"connected\" keys.
+
+Returns a formatted string of the device name, highlighted with
+the `secondary-selection` face if the device is currently connected.
+This is used for display in completing-read prompts."
   (let ((name (gethash "name" info))
         (connected (gethash "connected" info)))
     (format "%s"
             (if connected (propertize name 'face 'secondary-selection) name))))
 
 ;;; API Functions
+
 (defun blueutil-paired ()
+  "Return a list of all paired Bluetooth devices.
+
+Queries the blueutil utility for devices that have been previously
+paired with this Mac.  Each device is returned as a hash table
+containing keys such as \"address\", \"name\", \"connected\", etc.
+
+Example:
+  (blueutil-paired)
+  ;; => ((\"address\" . \"xx:xx:xx:xx:xx:xx\")
+  ;;     (\"name\" . \"AirPods\")
+  ;;     (\"connected\" . t)
+  ;;     ...)"
   (blueutil--run-json 'paired))
 
 (defun blueutil-connected ()
+  "Return a list of currently connected Bluetooth devices.
+
+Queries the blueutil utility for devices that are currently
+connected and active.  Each device is returned as a hash table
+containing keys such as \"address\", \"name\", etc.
+
+Example:
+  (blueutil-connected)
+  ;; => ((\"address\" . \"xx:xx:xx:xx:xx:xx\")
+  ;;     (\"name\" . \"Magic Mouse\")
+  ;;     ...)"
   (blueutil--run-json 'connected))
 
 (defun blueutil-connect (id)
+  "Connect to a Bluetooth device with the specified ID.
+
+ID is the unique address (MAC address) of the Bluetooth device
+to connect to, as a string.
+
+This command initiates a connection attempt to the device.  If the
+device is paired and in range, it should connect successfully.
+
+Example:
+  (blueutil-connect \"xx:xx:xx:xx:xx:xx\")
+  ;; => \"\" (empty string on success)"
   (blueutil--run 'connect (list id)))
 
 (defun blueutil-disconnect (id)
+  "Disconnect from a Bluetooth device with the specified ID.
+
+ID is the unique address (MAC address) of the Bluetooth device
+to disconnect from, as a string.
+
+This command terminates the active connection to the device.  The
+device remains paired and can be reconnected later.
+
+Example:
+  (blueutil-disconnect \"xx:xx:xx:xx:xx:xx\")
+  ;; => \"\" (empty string on success)"
   (blueutil--run 'disconnect (list id)))
 
 (defun blueutil-search-paired ()
+  "Interactively select and manage a paired Bluetooth device.
+
+This interactive command presents a completing-read interface to:
+1. Select a paired Bluetooth device from a list
+2. Choose an action (connect or disconnect) to perform on it
+
+Connected devices are highlighted in the selection list for easy
+identification.  After selecting a device and action, the appropriate
+command is executed to perform the operation.
+
+Example usage:
+  M-x blueutil-search-paired"
   (interactive)
   (when-let* ((pairs (blueutil-paired))
               (selected (completing-read "Select a bluetooth: " (mapcar #'blueutil--info-key pairs)))
